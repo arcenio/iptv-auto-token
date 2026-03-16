840271b12d80272573b95ef9b9ae2eb29f559775
arcenio <arceniogallardo@gmail.com>
2026-03-15 23:30:15 -0500

    Update update_lista.py

--- update_lista.py
+++ update_lista.py
@@ -23,7 +23,9 @@ with sync_playwright() as p:
     if m3u8_encontrado:
 
         contenido = "#EXTM3U\n"
-        contenido += "#EXTINF:-1,RCN En Vivo\n"
+        contenido += "#EXTINF:-1 tvg-id=\"rcn\" tvg-name=\"RCN\" group-title=\"TV\",RCN En Vivo\n"
+        contenido += "#EXTVLCOPT:http-referrer=https://www.cablevisionhd.com/\n"
+        contenido += "#EXTVLCOPT:http-user-agent=Mozilla/5.0\n"
         contenido += m3u8_encontrado + "\n"
 
         with open("lista.m3u","w",encoding="utf-8") as f:
