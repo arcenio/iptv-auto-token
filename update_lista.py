776f599ff323bb050f44d29ad19d0b7f19eaf87c
arcenio <arceniogallardo@gmail.com>
2026-03-16 21:42:23 -0500

    Update update_lista.py

--- update_lista.py
+++ update_lista.py
@@ -2,14 +2,14 @@ from playwright.sync_api import sync_playwright
 
 url = "https://www.cablevisionhd.com/rcn-en-vivo.html"
 
-m3u8_encontrado = None
+m3u8_lista = []
 
 def capturar_respuesta(response):
-    global m3u8_encontrado
+    global m3u8_lista
     enlace = response.url
 
-    if ".m3u8" in enlace and "token=" in enlace and "expires=" in enlace:
-        m3u8_encontrado = enlace
+    if ".m3u8" in enlace:
+        m3u8_lista.append(enlace)
 
 with sync_playwright() as p:
 
@@ -20,24 +20,25 @@ with sync_playwright() as p:
 
     page.goto(url, timeout=60000)
 
-    page.wait_for_timeout(15000)
+    page.wait_for_timeout(20000)
 
-    if m3u8_encontrado:
+    if m3u8_lista:
+
+        enlace_final = m3u8_lista[-1]
 
         contenido = "#EXTM3U\n"
         contenido += "#EXTINF:-1 tvg-id=\"rcn\" tvg-name=\"RCN\" group-title=\"TV\",RCN En Vivo\n"
         contenido += "#EXTVLCOPT:http-referrer=https://regionales.saohgdasregions.fun/\n"
         contenido += "#EXTVLCOPT:http-origin=https://regionales.saohgdasregions.fun\n"
         contenido += "#EXTVLCOPT:http-user-agent=Mozilla/5.0\n"
-        contenido += m3u8_encontrado + "\n"
+        contenido += enlace_final + "\n"
 
         with open("lista.m3u", "w", encoding="utf-8") as f:
             f.write(contenido)
 
-        print("M3U8 capturado correctamente:")
-        print(m3u8_encontrado)
+        print("M3U8 usado:", enlace_final)
 
     else:
-        print("No se encontro m3u8 con token")
+        print("No se detecto m3u8")
 
     browser.close()
